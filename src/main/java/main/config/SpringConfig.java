ccab0e2a56231fa83ffe3b30e086060b36430c0b
Bogino <ibogino@yandex.ru>
2021-05-17 01:00:55 +0500

    SpringConfig corrected

--- src/main/java/main/config/SpringConfig.java
+++ src/main/java/main/config/SpringConfig.java
@@ -2,6 +2,7 @@ package main.config;
 
 import main.model.repository.CaptchaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,7 +14,7 @@ public class SpringConfig {
     @Autowired
     CaptchaRepository captchaRepository;
 
-    @Scheduled(initialDelay = 3_600_000, fixedDelay = 3_600_000)
+    @Scheduled(fixedDelayString = "${blog.captchaLifeTimeInHours}")
     public void scheduleFixedDeleteCaptchas() {
         captchaRepository.deleteOldCaptchas();
     }
